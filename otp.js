d3776cc705bd10ad068bd2016e6608238cde3c4a
laloba36 <78861785+laloba36@users.noreply.github.com>
2021-10-15 14:57:05 +0530

    Add files via upload

---
+++ otp.js
@@ -0,0 +1,70 @@
+// $(function() {
+//     'use strict';
+  
+//     var body = $('body');
+  
+//     function goToNextInput(e) {
+//       var key = e.which,
+//         t = $(e.target),
+//         sib = t.next('input');
+  
+//       if (key != 9 && (key < 48 || key > 57)) {
+//         e.preventDefault();
+//         return false;
+        
+//       }
+  
+//       if (key === 9) {
+//         return true;
+//       }
+  
+//       if (!sib || !sib.length) {
+//         sib = body.find('input').eq(0);
+//       }
+//       sib.select().focus();
+//     }
+  
+//     function onKeyDown(e) {
+//       var key = e.which;
+  
+//       if (key === 9 || (key >= 48 && key <= 57)) {
+//         return true;
+//       }
+  
+//       e.preventDefault();
+//       return false;
+//     }
+    
+//     function onFocus(e) {
+//       $(e.target).select();
+//     }
+  
+//     body.on('keyup', 'input', goToNextInput);
+//     body.on('keydown', 'input', onKeyDown);
+//     body.on('click', 'input', onFocus);
+  
+//   })
+$('.digit-group').find('input').each(function() {
+  $(this).attr('maxlength', 1);
+  $(this).on('keyup', function(e) {
+    var parent = $($(this).parent());
+    
+    if(e.keyCode === 8 || e.keyCode === 37) {
+      var prev = parent.find('input#' + $(this).data('previous'));
+      
+      if(prev.length) {
+        $(prev).select();
+      }
+    } else if((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 96 && e.keyCode <= 105) || e.keyCode === 39) {
+      var next = parent.find('input#' + $(this).data('next'));
+      
+      if(next.length) {
+        $(next).select();
+      } else {
+        if(parent.data('autosubmit')) {
+          parent.submit();
+        }
+      }
+    }
+  });
+});
